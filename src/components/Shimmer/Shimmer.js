8c58080b6b4fa7c5ce75202a116fa1fcc655bc6f
shyju <shyju.vishu@gmail.com>
2023-02-12 19:56:56 +0530

    Updated Shimmer component

--- src/components/Shimmer/Shimmer.js
+++ src/components/Shimmer/Shimmer.js
@@ -1,8 +1,22 @@
+import Blank from '../../assets/img/blank.jpg';
 export const Shimmer = () => {
     return (
         <div className="restraunt-list">
             {
-                Array(10).fill("").map((e, index) => <div key={index} className='shimmer-card' ></div>)
+                // Array(10).fill("").map((e, index) => <div key={index} className='shimmer-card' ></div>)
+
+                Array(10).fill("").map((e, index) => (
+                    <div className='card' >
+                    <img src={Blank} />
+                    <div className="restraunt-name"></div>
+                    <div className="restraunt-cuisines"></div>
+                    <div className="details">
+                        <div className="rating"></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                 </div>
+                ))
             }
         </div>
     )
