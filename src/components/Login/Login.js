ab5f50c0542a1086ef89c59cf25c642492f45b93
shyju <shyju.vishu@gmail.com>
2023-02-17 18:45:27 +0530

    added partticles js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,10 +7,112 @@ import { login } from "../../redux/userSlice";
 import Logo from '../../assets/img/foodvilla.png'
 import './Login.css';
 import { Authlogin, AuthSignUp } from "../../auth/auth-config";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
 export const Login = () => {
-
-
+    
+    const PARTICLE_OPTIONS = {
+        "particles": {
+            "number": {
+                "value": 80,
+                "density": {
+                    "enable": true,
+                    "value_area": 800
+                }
+            },
+            "shape": {
+                "type": "circle",
+                "stroke": {
+                    "width": 0,
+                    "color": "#000000"
+                },
+                "polygon": {
+                    "nb_sides": 3
+                },
+                "image": {
+                    "src": "img/github.svg",
+                    "width": 100,
+                    "height": 100
+                }
+            },
+            "color": {
+                "value": "#000000"
+            },
+            "size": {
+                "value": 3,
+                "random": true,
+                "anim": {
+                    "enable": false,
+                    "speed": 40,
+                    "size_min": 0.1,
+                    "sync": false
+                }
+            },
+            "line_linked": {
+                "enable": true,
+                "distance": 150,
+                "color": "#000000",
+                "opacity": 0.4,
+                "width": 1
+            },
+            "move": {
+                "enable": true,
+                "speed": 6,
+                "direction": "none",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": false,
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "grab"
+                },
+                "onclick": {
+                    "enable": true,
+                    "mode": "push"
+                },
+                "resize": true
+            },
+            "modes": {
+                "grab": {
+                    "distance": 140,
+                    "line_linked": {
+                        "opacity": 1
+                    }
+                },
+                "bubble": {
+                    "distance": 400,
+                    "size": 40,
+                    "duration": 2,
+                    "opacity": 8,
+                    "speed": 3
+                },
+                "repulse": {
+                    "distance": 200,
+                    "duration": 0.4
+                },
+                "push": {
+                    "particles_nb": 4
+                },
+                "remove": {
+                    "particles_nb": 2
+                }
+            }
+        },
+        "retina_detect": true
+    }
+    
     const dispatch = useDispatch();
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
@@ -42,10 +144,26 @@ export const Login = () => {
         // dispatch(login())
     }
 
+    const particlesInit = async (main) => {
+        console.log(main);
+    
+        // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        await loadFull(main);
+      };
+
     return (
         <>
             <ToastContainer/>
             <div className="login-container">
+            <Particles
+                id="tsparticles"
+                init={particlesInit}
+
+                options={PARTICLE_OPTIONS}
+            >
+            </Particles>
             <div className="login">
                 <img src={Logo} />
                 <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)}></input>
